cac4b5468fd5c1424665ae32d9863a8179d7abf5
Madison Coleman <31259524+madisonjosephcoleman@users.noreply.github.com>
2019-12-17 14:46:09 -0600

    firewall module

---
+++ modules/azure/azure-firewall/variables.tf
@@ -0,0 +1,59 @@
+variable "subnet_id" {
+  type = string
+}
+
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+
+
+# main
+variable "business_unit" {
+  description = "Business unit for the project - Portal name or business unit name"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name. e.g. 'Gov2Go'"
+  type        = string
+}
+
+variable "application_name" {
+  description = "Application name, e.g. 'app' or 'jenkins"
+  type        = string
+}
+
+variable "managed_by" {
+  description = "Organization team name or group. e.g. 'ETS-SRE'"
+  type        = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "delimiter" {
+  default = "-"
+}
+
+variable "attributes" {
+  type    = list(string)
+  default = []
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "enabled" {
+  type        = bool
+  description = "Whether to create this resource or not?"
+  default     = true
+}
